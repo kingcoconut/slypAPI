46a7078ac3a1cb4e834d1a9f56a67eb5c562f9a2
James Groeneveld <jgroeneveld@infleciton.com>
2015-08-14 08:36:01 -0700

    exposing and tested users array on slyp model

--- models/slyp.rb
+++ models/slyp.rb
@@ -4,11 +4,10 @@ require './models/user_slyp.rb'
 class Slyp < ActiveRecord::Base
   has_many :user_slyps
   has_many :users, through: :user_slyps
+  has_many :slyp_chats
 
   belongs_to :topic
-
   enum slyp_type: [:video, :article]
-
   class Entity < Grape::Entity
     expose :id
     expose :title
@@ -27,5 +26,33 @@ class Slyp < ActiveRecord::Base
     expose :engaged do |slyp, options|
       UserSlyp.where(slyp_id: slyp.id, user_id: options[:env]["api.endpoint"].cookies["user_id"].to_i).first.engaged
     end
+    expose :users do |slyp, options|
+      user_id = options[:env]["api.endpoint"].cookies["user_id"].to_s
+      slyp_id = slyp.id.to_s
+      #select distinct U.email  from slyp_chat_users SCU join users U on (SCU.user_id = U.id) where SCU.slyp_chat_id in (select distinct SCU.slyp_chat_id from slyp_chats SC join slyp_chat_users SCU on (SC.id = SCU.slyp_chat_id) where SCU.user_id = 1 and SC.slyp_id = 7) and U.id <> 1;
+      # TODO: replace this sql with cleaner activerecord relational mapping utilities
+      sql = "select distinct U.email "\
+      +"from slyp_chat_users SCU "\
+      +"join users U "\
+      +"on (SCU.user_id = U.id) "\
+      +"where SCU.slyp_chat_id in ("\
+        +"select distinct SCU.slyp_chat_id "\
+        +"from slyp_chats SC "\
+        +"join slyp_chat_users SCU "\
+        +"on (SC.id = SCU.slyp_chat_id) "\
+        +"where SCU.user_id="+user_id+" and SC.slyp_id="+slyp_id+") "\
+      +"and U.id <> "+user_id
+      ActiveRecord::Base.connection.select_all(sql)
+    end
   end
 end
+
+
+
+
+
+
+
+
+
+
