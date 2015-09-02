2c77608e9b61c77e3cd2e350f257d6192cb15673
Ubuntu <ubuntu@ip-172-31-12-163.us-west-1.compute.internal>
2015-09-02 05:30:10 +0000

    schema.rb syncing from server

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823200441) do
+ActiveRecord::Schema.define(version: 20150824053830) do
 
   create_table "keywords", force: :cascade do |t|
     t.string   "keyword",    limit: 255
